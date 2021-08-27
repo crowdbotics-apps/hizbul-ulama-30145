5d6b6cce160001174d524ffd44a0e6402555c8d5
Crowdbotics <team@crowdbotics.com>
2021-08-27 06:16:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "hizbul_ulama_30145Identifier",
+  url: "https://hizbul_ulama_30145Identifier.botics.co",
+  api: "https://hizbul_ulama_30145Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
